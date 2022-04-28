e0c84f8be172c8d2f6afa9e0283fdac562e24326
Vicotr12 <andreipnk2004@gmail.com>
2022-04-28 22:30:07 +0300

    domn colonel

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -12,7 +12,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: Text("Test123"),
+        child: Text("Cosmin colonel"),
       ),
     );
   }
