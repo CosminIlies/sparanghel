ae6f92d93d276f3b323243d2a9a712950036ecdc
CosminIlies <cosminilies1234@gmail.com>
2022-04-29 00:08:11 +0300

    adding leaderboardPage

---
+++ lib/pages/leaderboard.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class LeaderboardPage extends StatefulWidget {
+  const LeaderboardPage({Key? key}) : super(key: key);
+
+  @override
+  State<LeaderboardPage> createState() => _LeaderboardPageState();
+}
+
+class _LeaderboardPageState extends State<LeaderboardPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+
+        
+
+      ],
+    );
+  }
+}
